615f8926e5effa9d820e9c368a1d548c1f985bb2
VenkataSaiSankeerthReddyYama <sankeerthreddy2002@gmail.com>
2025-02-01 10:33:01 +0530

    commit

---
+++ wipro_javaee_project/src/wipro_javaee_project/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package wipro_javaee_project;
+
+/**
+ * @author Sankeerth reddy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
